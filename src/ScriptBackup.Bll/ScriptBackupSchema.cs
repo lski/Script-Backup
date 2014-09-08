da0021faa99b13999bbf2d8a2f581d88c6be6b18
Lee Cooper <lee.cooper@lski.uk>
2014-09-08 21:04:57 +0100

    Changed the concept of the schema backup to understane dependency ordering

--- src/ScriptBackup.Bll/ScriptBackupSchema.cs
+++ src/ScriptBackup.Bll/ScriptBackupSchema.cs
@@ -93,39 +93,69 @@ namespace ScriptBackup.Bll {
 				conn = new ServerConnection(sqlServer);
 
 				var svr = new Server(conn);
+				var scr = new Scripter(svr) {
+					Options = _scriptOptions
+				};
+				var walker = new DependencyWalker(svr);
 
 				var dbs = ResolveDatabases(svr, databases);
 
 				foreach (Database db in dbs) {
 
-					if (Options.CreateDatabase) {
-						ProcessDatabaseScript(db, iterator);
-					}
+					ProcessDatabaseScript(db, iterator);
+
+					var objects = new List<SqlSmoObject>();
 
 					if (Options.ScriptTables) {
-						ProcessTablesScript(ops, db, ResolveTables(db, tables), iterator);
+						objects.AddRange(ResolveTables(db, tables));
 					}
 
 					if (Options.ScriptViews) {
-						ProcessViewsScript(ops, db, ResolveViews(svr, db), iterator);
+						objects.AddRange(ResolveViews(svr, db));
 					}
 
 					if (Options.ScriptProcedures) {
-						ProcessProceduresScript(ops, db, ResolveProcedures(db), iterator);
+						objects.AddRange(ResolveProcedures(db));
 					}
 
 					if (Options.ScriptUdfs) {
-						ProcessUdfsScript(ops, db, ResolveUDFs(db), iterator);
+						objects.AddRange(ResolveUDFs(db));
 					}
 
 					if (Options.ScriptPartitionFunctions) {
-						ProcessPartitionFunctionsScript(ops, db, ResolvePartitionFunctions(db), iterator);
+						objects.AddRange(ResolvePartitionFunctions(db));
 					}
 
 					if (Options.ScriptPartitionSchemes) {
-						ProcessPartitionSchemesScript(ops, db, ResolvePartitionSchemes(db), iterator);
+						objects.AddRange(ResolvePartitionSchemes(db));
+					}
+
+					IEnumerable<Mini> orderedLst = null;
+
+					if (true) {
+
+						orderedLst = objects.Select(obj => new Mini() {
+							Name = obj.Urn.GetAttribute("Name"),
+							Type = obj.Urn.Type,
+							SmoObject = obj
+						});
+					} else {
+
+						var tree = scr.DiscoverDependencies(objects.ToArray(), false);
+						var coll = walker.WalkDependencies(tree).ToList();
+
+						orderedLst = coll.Select(dep => new Mini() {
+							Name = dep.Urn.GetAttribute("Name"),
+							Type = dep.Urn.Type,
+							SmoObject = svr.GetSmoObject(dep.Urn)
+						});
+					}
+
+					foreach (var mini in orderedLst) {
+						ProcessScript(scr, db.Name, mini, iterator);
 					}
 				}
+
 			} finally {
 
 				if (conn != null) {
@@ -134,163 +164,33 @@ namespace ScriptBackup.Bll {
 			}
 		}
 
-		private void ProcessPartitionSchemesScript(ScriptingOptions ops, Database db, IEnumerable<PartitionScheme> partitionSchemes, Action<string, string, string, string> iterator) {
+		private class Mini {
 
-			foreach (var partitionScheme in partitionSchemes) {
+			public string Name { get; set; }
 
-				Console.WriteLine("PartitionScheme: " + partitionScheme.Name);
+			public string Type { get; set; }
 
-				var output = partitionScheme.Script(ops);
-
-				var sb = new StringBuilder();
-
-				foreach (string st in output) {
-					sb.AppendLine(st).AppendLine("GO");
-				}
-
-				iterator(sb.ToString(), db.Name, partitionScheme.Name, typeof(PartitionScheme).Name);
-			}
+			public SqlSmoObject SmoObject { get; set; }
 		}
 
-		private IEnumerable<PartitionScheme> ResolvePartitionSchemes(Database db) {
+		private void ProcessScript(Scripter scr, string db, Mini data, Action<string, string, string, string> iterator) {
 
-			if (db.CompatibilityLevel <= CompatibilityLevel.Version80) {
-				return Enumerable.Empty<PartitionScheme>();
+			var name = data.SmoObject.Urn.GetAttribute("Name");
+			var type = data.SmoObject.Urn.Type;
+			var obj = data.SmoObject;
+
+			Console.WriteLine(type + ": " + name);
+
+			var output = scr.Script(new[] { obj });
+
+			var sb = new StringBuilder();
+
+			foreach (string st in output) {
+				sb.AppendLine(st)
+					.AppendLine("GO");
 			}
 
-			return db.PartitionSchemes.Cast<PartitionScheme>();
-		}
-
-		private void ProcessPartitionFunctionsScript(ScriptingOptions ops, Database db, IEnumerable<PartitionFunction> partitionFuncs, Action<string, string, string, string> iterator) {
-
-			foreach (var partitionFunc in partitionFuncs) {
-
-				Console.WriteLine("PartitionFunction: " + partitionFunc.Name);
-
-				var output = partitionFunc.Script(ops);
-
-				var sb = new StringBuilder();
-
-				foreach (string st in output) {
-					sb.AppendLine(st).AppendLine("GO");
-				}
-
-				iterator(sb.ToString(), db.Name, partitionFunc.Name, typeof(PartitionFunction).Name);
-			}
-		}
-
-		private IEnumerable<PartitionFunction> ResolvePartitionFunctions(Database db) {
-
-			if (db.CompatibilityLevel <= CompatibilityLevel.Version80) {
-				return Enumerable.Empty<PartitionFunction>();
-			}
-
-			return db.PartitionFunctions.Cast<PartitionFunction>();
-		}
-
-		private void ProcessUdfsScript(ScriptingOptions ops, Database db, IEnumerable<UserDefinedFunction> udfs, Action<string, string, string, string> iterator) {
-
-			foreach (var udf in udfs) {
-
-				Console.WriteLine("UDF: " + udf.Name);
-
-				var output = udf.Script(ops);
-
-				var sb = new StringBuilder();
-
-				foreach (string st in output) {
-					sb.AppendLine(st).AppendLine("GO");
-				}
-
-				iterator(sb.ToString(), db.Name, udf.Name, typeof(UserDefinedFunction).Name);
-			}
-		}
-
-		private IEnumerable<UserDefinedFunction> ResolveUDFs(Database db, IEnumerable<string> udfs = null) {
-
-			var qry = db.UserDefinedFunctions.Cast<UserDefinedFunction>().Where(t => !t.IsSystemObject);
-
-			if (udfs != null) {
-				qry = qry.Where(t => udfs.Contains(t.Name));
-			}
-
-			return qry;
-		}
-
-		private void ProcessViewsScript(ScriptingOptions ops, Database db, IEnumerable<View> views, Action<string, string, string, string> iterator) {
-
-			foreach (var view in views) {
-
-				Console.WriteLine("View: " + view.Name);
-
-				var output = view.Script(ops);
-
-				var sb = new StringBuilder();
-
-				foreach (string st in output) {
-					sb.AppendLine(st).AppendLine("GO");
-				}
-
-				iterator(sb.ToString(), db.Name, view.Name, typeof(View).Name);
-			}
-		}
-
-		private IEnumerable<View> ResolveViews(Server svr, Database db, IEnumerable<string> views = null) {
-
-			var qry = db.Views.Cast<View>().Where(t => !t.IsSystemObject);
-
-			if (views != null) {
-				qry = qry.Where(t => views.Contains(t.Name));
-			}
-
-			//var scr = new Scripter(svr);
-			//var walker = new DependencyWalker(svr);
-
-			//var tree = scr.DiscoverDependencies(qry.Cast<SqlSmoObject>().ToArray(), false);
-			//var coll = walker.WalkDependencies(tree);
-
-			return qry;
-		}
-
-		private void ProcessProceduresScript(ScriptingOptions ops, Database db, IEnumerable<StoredProcedure> sps, Action<string, string, string, string> iterator) {
-
-			foreach (var sp in sps) {
-
-				Console.WriteLine("Procedure: " + sp.Name);
-
-				var output = sp.Script(ops);
-
-				var sb = new StringBuilder();
-
-				foreach (string st in output) {
-					sb.AppendLine(st).AppendLine("GO");
-				}
-
-				iterator(sb.ToString(), db.Name, sp.Name, typeof(StoredProcedure).Name);
-			}
-		}
-
-		private IEnumerable<StoredProcedure> ResolveProcedures(Database db) {
-
-			return db.StoredProcedures.Cast<StoredProcedure>().Where(t => !t.IsSystemObject);
-		}
-
-		private void ProcessTablesScript(ScriptingOptions ops, Database db, IEnumerable<Table> tabs, Action<string, string, string, string> iterator) {
-
-			foreach (var tab in tabs) {
-
-				Console.WriteLine("Table: " + tab.Name);
-
-				var output = tab.EnumScript(ops);
-
-				var sb = new StringBuilder();
-
-				foreach (string st in output) {
-					sb.AppendLine(st).AppendLine("GO");
-				}
-
-				iterator(sb.ToString(), db.Name, tab.Name, typeof(Table).Name);
-			}
+			iterator(sb.ToString(), db, name, type);
 		}
 
 		private void ProcessDatabaseScript(Database db, Action<string, string, string, string> iterator) {
@@ -314,6 +214,106 @@ namespace ScriptBackup.Bll {
 			sb.AppendLine("GO");
 
 			iterator(sb.ToString(), db.Name, db.Name, typeof(Database).Name);
+		}
+
+		//public void Process(Action<string, string, string, string> iterator) {
+
+		//	var sqlServer = Options.ServerName;
+		//	var ops = _scriptOptions;
+		//	var databases = _options.Databases;
+		//	var tables = _options.Tables;
+
+		//	ServerConnection conn = null;
+
+		//	try {
+
+		//		conn = new ServerConnection(sqlServer);
+
+		//		var svr = new Server(conn);
+
+		//		var dbs = ResolveDatabases(svr, databases);
+
+		//		foreach (Database db in dbs) {
+
+		//			if (Options.CreateDatabase) {
+		//				ProcessDatabaseScript(db, iterator);
+		//			}
+
+		//			if (Options.ScriptTables) {
+		//				ProcessTablesScript(ops, db, ResolveTables(db, tables), iterator);
+		//			}
+
+		//			if (Options.ScriptProcedures) {
+		//				ProcessProceduresScript(ops, db, ResolveProcedures(db), iterator);
+		//			}
+
+		//			if (Options.ScriptUdfs) {
+		//				ProcessUdfsScript(ops, db, ResolveUDFs(db), iterator);
+		//			}
+
+		//			if (Options.ScriptViews) {
+		//				ProcessViewsScript(ops, db, ResolveViews(svr, db), iterator);
+		//			}
+
+		//			if (Options.ScriptPartitionFunctions) {
+		//				ProcessPartitionFunctionsScript(ops, db, ResolvePartitionFunctions(db), iterator);
+		//			}
+
+		//			if (Options.ScriptPartitionSchemes) {
+		//				ProcessPartitionSchemesScript(ops, db, ResolvePartitionSchemes(db), iterator);
+		//			}
+		//		}
+		//	} finally {
+
+		//		if (conn != null) {
+		//			conn.Disconnect();
+		//		}
+		//	}
+		//}
+		private IEnumerable<PartitionScheme> ResolvePartitionSchemes(Database db) {
+
+			if (db.CompatibilityLevel <= CompatibilityLevel.Version80) {
+				return Enumerable.Empty<PartitionScheme>();
+			}
+
+			return db.PartitionSchemes.Cast<PartitionScheme>();
+		}
+
+		private IEnumerable<PartitionFunction> ResolvePartitionFunctions(Database db) {
+
+			if (db.CompatibilityLevel <= CompatibilityLevel.Version80) {
+				return Enumerable.Empty<PartitionFunction>();
+			}
+
+			return db.PartitionFunctions.Cast<PartitionFunction>();
+		}
+
+		private IEnumerable<UserDefinedFunction> ResolveUDFs(Database db, IEnumerable<string> udfs = null) {
+
+			var qry = db.UserDefinedFunctions.Cast<UserDefinedFunction>().Where(t => !t.IsSystemObject);
+
+			if (udfs != null) {
+				qry = qry.Where(t => udfs.Contains(t.Name));
+			}
+
+			return qry;
+		}
+
+		private IEnumerable<View> ResolveViews(Server svr, Database db, IEnumerable<string> views = null) {
+
+			var qry = db.Views.Cast<View>().Where(t => !t.IsSystemObject);
+
+			if (views != null) {
+				qry = qry.Where(t => views.Contains(t.Name));
+			}
+
+			return qry
+				;
+		}
+
+		private IEnumerable<StoredProcedure> ResolveProcedures(Database db) {
+
+			return db.StoredProcedures.Cast<StoredProcedure>().Where(t => !t.IsSystemObject);
 		}
 	}
 }
